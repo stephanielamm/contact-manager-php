7af9a24c61ddbc3336beee4a029c02bb83b49ac7
stephanielamm <slamm5@live.unc.edu>
2017-03-02 16:16:22 -0500

    Add Contact form linked to button.
    Will need to move to this a pop-up of some form so that the user will
    not have to go back.

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php
   include 'head.php';
 ?>
-<!DOCTYPE html>
-<html>
- <head>
-   <meta charset="utf-8">
-   <!-- bootstrap -->
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<link rel="stylesheet" source="style.css">
-   <title>Contact Manager</title>
- </head>
- <body>
-   <h1>Contacts</h1>
-  <!-- CONTACT LIST -->
-  <div class="container-fluid contact-list">
-  </div>
 
- </body>
-</html>
+<div class="container-fluid">
+  <a href="add.php">
+    Add Contact
+  </a>
+</div>
+
+<div class="container-fluid">
+  <?php foreach($contacts as $contact) : ?>
+    <?= $contact['firstname']; ?> <?= $contact['lastname']; ?>
+</div>
+<?php endforeach ?>
+
+<?php include 'footer.php';
+?>
