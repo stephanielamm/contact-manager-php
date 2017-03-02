bc556b9be1fb47be0f9851f9e147ac721547df7d
stephanielamm <slamm5@live.unc.edu>
2017-03-02 16:56:59 -0500

    Switched back to bootstrap and got it working.

--- add.php
+++ add.php
@@ -1,6 +1,7 @@
 <?php
 ?>
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 <!-- CREATE NEW CONTACT -->
 <form>
   <div class="form-group">
