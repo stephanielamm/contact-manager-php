33b648f54f2befd1148251c4cc801a0c8228b120
stephanielamm <slamm5@live.unc.edu>
2017-03-02 15:25:09 -0500

    Moved New Contact Form to add.php.
    Transitioned the new contact form to add.php.

--- index.php
+++ index.php
@@ -14,36 +14,14 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<link rel="stylesheet" source="style.css"> 
+<link rel="stylesheet" source="style.css">
    <title>Contact Manager</title>
  </head>
  <body>
    <h1>Contacts</h1>
-
   <!-- CONTACT LIST -->
   <div class="container-fluid contact-list">
   </div>
-  <!-- CREATE NEW CONTACT -->
-<form>
-<div class="form-group">
-  <label>Name</label>
-  <input type="name" class="form-control" id="name" placeholder="Enter Name">
-</div>
-<div class="form-group">
-  <label>Phone Number</label> <!-- Will allow for cell, home, and add button for additional phone number -->
-  <input type="phone" class="form-control" id="phone" placeholder="Enter Phone Number">
-</div>
-<div class="form-group">
-  <label>Email Address</label> <!-- Will add button for additional email -->
-  <input type="email" class="form-control" id="email" placeholder="Enter Email">
-</div>
-<div class="form-group">
-  <label>Notes</label>
-  <textarea class="form-control" id="notes" rows="3"></textarea>
-</div>
-<button type="submit" class="btn btn-primary">Add</button>
-</form>
-<!-- END CREATE NEW CONTACT -->
 
  </body>
 </html>
