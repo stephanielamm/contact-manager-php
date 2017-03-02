1b21012f4013d3e132f490c450b982b61922d4ea
stephanielamm <slamm5@live.unc.edu>
2017-03-02 14:34:31 -0500

    Added bootstrap.
    Adding bootstrap files and style.css.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-$db = new PDO(‘mysql:host=localhost;dbname=nameofthedateabase;charset=utf8mb4’, ‘username’, ‘password’);
+//  $db = new PDO(‘mysql:host=localhost;dbname=nameofthedateabase;charset=utf8mb4’, ‘username’, ‘password’);
 ?>
+<!DOCTYPE html>
+<html>
+ <head>
+   <meta charset="utf-8">
+   <title></title>
+ </head>
+ <body>
+ </body>
+</html>
